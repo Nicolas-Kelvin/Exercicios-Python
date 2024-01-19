d3a15801367597526be5e442955fa22e791d8401
Nícolas Kelvin <nicolaskelvinsilva@gmail.com>
2024-01-19 08:28:41 -0300

    add exrcicíos

---
+++ ex005.py
@@ -0,0 +1,2 @@
+#Faça um programa que leia um número inteiro e montre na tela o seu sucessor e seu antecessor. 
+
---
+++ ex011.py
@@ -0,0 +1,1 @@
+#Faça um programa que leia a largura e a altura de uma parede em metros, calcule a sua área e a quantidade de tinta necessária para pinta-lá, sabedo que cada litro de tinta, pinta uma área de 2m2.
