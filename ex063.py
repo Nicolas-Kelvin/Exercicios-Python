8101e173f5b986cf494b759de51e46d7684a066f
Nícolas K. <87534075+Nicolas-Kelvin@users.noreply.github.com>
2024-04-11 16:48:26 -0300

    att exercicios

---
+++ ex063.py
@@ -0,0 +1,8 @@
+'''Escreva um programa que leia um número N inteiro qualquer e mostre na tela os N primeiros elementos de uma Sequência de Fibonacci. Exemplo:
+
+0 – 1 – 1 – 2 – 3 – 5 – 8'''
+
+print('-' * 26)
+print('Sequência de Fibonacci')
+print('-' * 26)
+n = int(input('Quantos termos você quer mostrar? '))
