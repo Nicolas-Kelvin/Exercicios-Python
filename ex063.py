8a1f2dfc7763bc0001039a61e82348eb4d4f1596
Nícolas K. <87534075+Nicolas-Kelvin@users.noreply.github.com>
2024-04-18 16:23:30 -0300

    att exercicios

--- ex063.py
+++ ex063.py
@@ -2,7 +2,20 @@
 
 0 – 1 – 1 – 2 – 3 – 5 – 8'''
 
-print('-' * 26)
+print('-' * 40)
 print('Sequência de Fibonacci')
-print('-' * 26)
+print('-' * 40)
 n = int(input('Quantos termos você quer mostrar? '))
+t1 = 0
+t2 = 1
+print('~' * 40)
+print('{} ⤳ {}'.format(t1, t2), end='')
+cont= 3
+while cont <= n:
+    t3 = t1 + t2
+    print(' ⤳ {}'.format(t3), end='')
+    t1 = t2
+    t2 = t3
+    cont += 1
+print(' ⤳ FIM')
+print('~' * 40)
