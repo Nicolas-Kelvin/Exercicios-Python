e614eccd82141074ae635853cb7a575ed7628057
Nícolas K. <87534075+Nicolas-Kelvin@users.noreply.github.com>
2024-04-18 18:21:05 -0300

    att exercicios

--- ex065.py
+++ ex065.py
@@ -1,13 +1,20 @@
 '''Crie um programa que leia vários números inteiros pelo teclado. No final da execução, mostre a média entre todos os valores e qual foi o maior e o menor valores lidos. O programa deve perguntar ao usuário se ele quer ou não continuar a digitar valores.'''
-n = soma = cont = 0
+n = soma = cont = quant = maior = menor = 0
 cond = 'S'
-
 while cond != 'N':
-    n = float(input('Digite um número: '))
+    n = int(input('Digite um número: '))
     soma += n
     cont += 1
+    quant += 1
     cond = str(input('Quer continuar? [S/N] ')).strip() .upper()
     media = soma / cont
-print('Você digitou foi {} números e a média foi {:.2f}'.format(cont, media))
-print('O maior valor foi {:.0f} e o menor foi {:.0f}'.format(max(n, n), min(n, n)))
+    if quant == 1:
+        maior = menor = n
+    else:
+        if n > maior:
+            maior = n
+        if n < menor:
+            menor = n
+print('Você digitou foi {} números e a média foi {}'.format(cont, media))
+print('O maior valor foi {} e o menor foi {}'.format(maior, menor))
 
